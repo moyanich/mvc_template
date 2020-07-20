20df7081fffc7c9ab0a623c63e6285bd6f1d6d1e
Amoy N <moyanich@users.noreply.github.com>
2020-07-20 16:04:10 -0500

    Updated core class

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,21 +4,3 @@ require_once 'libraries/core.class.php';
 require_once 'libraries/controller.class.php';
 require_once 'libraries/database.class.php';
 
-/*
-spl_autoload_register('myAutoloader', false);
-
-spl_autoload_register('myAutoloader');
-
-function myAutoloader($className) {
-    $path = "libraries/";
-    $extension = ".class.php";
-    $fullPath = $path . $className . $extension;
-
-    if (!file_exists($fullPath)) :
-        //echo "File does not exist";
-        return false;
-    else: 
-        require_once $fullPath;
-    endif; 
-}
-*/
