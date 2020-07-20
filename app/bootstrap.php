8d90a712c993442e6de6e90db4e6c146cc69990c
Amoy N <moyanich@users.noreply.github.com>
2020-07-20 15:46:48 -0500

    url parameters
    Added url parameters

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,7 +1,24 @@
-//require all the files we need
-
 <?php
-    // Load Libraries
-    require_once 'libraries/core.php';
-    require_once 'libraries/controller.php';
-    require_once 'libraries/database.php';
+
+require_once 'libraries/core.class.php';
+require_once 'libraries/controller.class.php';
+require_once 'libraries/database.class.php';
+
+/*
+spl_autoload_register('myAutoloader', false);
+
+spl_autoload_register('myAutoloader');
+
+function myAutoloader($className) {
+    $path = "libraries/";
+    $extension = ".class.php";
+    $fullPath = $path . $className . $extension;
+
+    if (!file_exists($fullPath)) :
+        //echo "File does not exist";
+        return false;
+    else: 
+        require_once $fullPath;
+    endif; 
+}
+*/
